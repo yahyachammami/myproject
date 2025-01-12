a9fb006db4c6c42b3d5992d72838c33d17ec8de2
Chammami Yahia <chammemiyahia@gmail.com>
2025-01-12 21:22:07 +0100

    Update app.js
    add userName
    
    Co-Authored-By: Jemmy778 <194714383+Jemmy778@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // Basic "Hello, World!" example
 console.log("Hello, World!");
+// Ask for the user's name and greet them
+let userName = prompt("Enter your name:");
+console.log("Hello, " + userName + "!");
